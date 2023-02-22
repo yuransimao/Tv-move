155644727ce7acc5395e65493fd493a157c11425
Yuran Simao <yuransimao7@gmail.com>
2023-02-22 13:50:17 +0100

    adding Navbar

--- src/Components/NavBar/navbar.tsx
+++ src/Components/NavBar/navbar.tsx
@@ -1,8 +1,17 @@
 import {Link} from "react-router-dom"
+import Styles from "./styles.module.sass"
 export function Navbar(){
     return (
-        <ul>
+        <div>
+            <div className={Styles.Navbar_list}>
+            <ul>
             <li><Link to="/">Home</Link></li>
-        </ul>
+            <li><Link to="/Movies">Movies</Link></li>
+            <li><Link to="/TVShows">Tv Shows</Link></li>
+            <li><Link to="/Watchlist">Watchlist</Link></li>
+            <li><Link to="/Geners">Geners</Link></li>
+            </ul>
+            </div>
+        </div>
     )
 }
