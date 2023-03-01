a2d0f562726e57d3f46d7b1354ce65ad66675397
Yuran Simao <yuransimao7@gmail.com>
2023-03-02 00:19:49 +0100

    refactoring code part6

---
+++ src/Hooks/useVideo.tsx
@@ -0,0 +1,25 @@
+import { useParams } from "react-router-dom"
+import { useState, useEffect} from "react";
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+export function GETVIDEO (){
+    const {id} = useParams()
+    const [video , setVideo] = useState()
+    
+    const getVideo = async (url : any) =>{
+        const resp = await fetch(url)
+        const data = await resp.json()
+        setVideo(data)
+    }
+
+    useEffect(()=>{
+        const VideoUrl = `${moviesURL}${id}/video?${apiKey}`
+        getVideo(VideoUrl)
+    },[id])
+
+    return{
+        Videosuccess: video?.sucess === true,
+        video
+    }
+}
--- src/Pages/Video/video.tsx
+++ src/Pages/Video/video.tsx
@@ -1,28 +1,15 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect} from "react";
+import {GETVIDEO} from "../../Hooks/useVideo"
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 
 export function Videos(){
-
-    const {id} = useParams()
-    const [video , setVideo] = useState()
-    
-    const getVideo = async (url : any) =>{
-        const resp = await fetch(url)
-        const data = await resp.json()
-        setVideo(data)
-    }
-
-    useEffect(()=>{
-        const VideoUrl = `${moviesURL}${id}/video?${apiKey}`
-        getVideo(VideoUrl)
-    },[id])
-
-    console.log(video)
+    const {Videosuccess} = GETVIDEO()
+   
 
     return (<>
-    {video?.sucess === true ? <h1>Video sucess</h1> : <h1>Not video success</h1>}
+    {Videosuccess ? <h1>Video sucess</h1> : <h1>Not video success</h1>}
     </>)
 }
