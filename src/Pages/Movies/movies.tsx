36f638615c47d41e8dbe9900dbb008b0b4d4b10c
Yuran Simao <yuransimao7@gmail.com>
2023-03-03 15:33:16 +0100

    getting movies popular

--- src/Pages/Movies/movies.tsx
+++ src/Pages/Movies/movies.tsx
@@ -1,57 +1,77 @@
-import { Swipert} from "../../Components/Slider/Swiper/swipert";
-import { Swipers} from "../../Components/Slider/Swiper/swiper";
-import {useState, useEffect} from "react"
+import { Swipert } from "../../Components/Slider/Swiper/swipert";
+import { Swipers } from "../../Components/Slider/Swiper/swiper";
+import { useState, useEffect } from "react"
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
- 
 
 
-export function Movies(){
-    const [cinemas, setCinemas] =useState([])
-    const [upcoming, setUpcoming] =useState([])
-    
-    const getCinema = async (url :any) =>{
+
+export function Movies() {
+    const [cinemas, setCinemas] = useState([])
+    const [upcoming, setUpcoming] = useState([])
+    const [popular, setPopular] = useState([])
+
+    const getCinema = async (url: any) => {
         const resp = await fetch(url)
         const data = await resp.json()
 
         setCinemas(data.results)
     }
 
-    const getupcoming = async(url : any) => {
-        const  resp = await fetch (url)
+    const getupcoming = async (url: any) => {
+        const resp = await fetch(url)
         const data = await resp.json()
         setUpcoming(data.results)
     }
+    const getpopular = async (url: any) => {
+        const resp = await fetch(url)
+        const data = await resp.json()
+        setPopular(data.results)
+    }
 
-    useEffect(() =>{
+    useEffect(() => {
         const url = `${moviesURL}now_playing?${apiKey}`
-        const Urlupcoming =`${moviesURL}upcoming?${apiKey}`
+        const Urlupcoming = `${moviesURL}upcoming?${apiKey}`
+        const Urlpopular = `${moviesURL}popular?${apiKey}`
         getCinema(url)
         getupcoming(Urlupcoming)
+        getpopular(Urlpopular)
     }, [])
 
-    const id = cinemas.map((cinemas :any) => {
+    const id = cinemas.map((cinemas: any) => {
         const id = cinemas.id
         return id
     })
-    const idupcoming = upcoming.map((cinemas :any) => {
+    const idupcoming = upcoming.map((cinemas: any) => {
         const id = cinemas.id
         return id
     })
-    const poster_path = cinemas.map((cinemas : any) => {
+    const idpopular = popular.map((cinemas: any) => {
+        const id = cinemas.id
+        return id
+    })
+    const poster_path = cinemas.map((cinemas: any) => {
         const poster_path = cinemas.poster_path
         console.log(upcoming)
         return poster_path
-        
-            })
-    const poster_pathupcoming= upcoming.map((cinemas : any) => {
+
+    })
+
+    const poster_pathupcoming = upcoming.map((cinemas: any) => {
         const poster_path = cinemas.poster_path
         console.log(upcoming)
         return poster_path
-        
-            })
+
+    })
+
+    const poster_pathpopular = popular.map((cinemas: any) => {
+        const poster_path = cinemas.poster_path
+        console.log(upcoming)
+        return poster_path
+
+    })
 
 
 
@@ -59,8 +79,9 @@ export function Movies(){
     console.log(upcoming)
     return (
         <>
-        <Swipert id ={id} poster_path={poster_path}/>
-        <Swipers title ={'Filme popular'} id ={idupcoming} poster_path={poster_pathupcoming}/>
+            <Swipert id={id} poster_path={poster_path} />
+            <Swipers title={'Filme Popular'} id={idpopular} poster_path={poster_pathpopular} />
+            <Swipers title={'Filme Upcoming'} id={idupcoming} poster_path={poster_pathupcoming} />
         </>
     )
 }
